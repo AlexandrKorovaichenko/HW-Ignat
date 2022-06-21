5edfe96ba57359aa2a7bf81246763e47b44880b8
Alexandr <puls.ek@rambler.ru>
2022-06-21 21:30:36 +0300

    Небольшие правки

--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -14,7 +14,7 @@ function Pages() {
             {/*Routes выбирает первый подходящий роут*/} 
             <Routes>
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                {/*<Route path = {'/'} element = {<Navigate to = {PATH.LITTLE_JUNIOR}/>}/>*/}
+                <Route path = {'/'} element = {<Navigate to = {PATH.LITTLE_JUNIOR}/>}/>
                 <Route path = {PATH.LITTLE_JUNIOR} element = {<PreJunior />}/>
                 {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
                 {/*<Route path={'/*'} element={<Error404/>}/>*/}
